28b5627057142ffc2bbc9c1db4783aa693efa1fc
Michele <gerunmic@students.zhaw.ch>
2015-01-06 19:34:42 +0100

    Menus as arrays

--- wordpress/wp-content/themes/trackstartheme/functions.php
+++ wordpress/wp-content/themes/trackstartheme/functions.php
@@ -1,9 +1,12 @@
 <?php
 
 function register_my_menus() {
-
-    register_nav_menu('header-menu', __('Main'));
-    register_nav_menu('footer-menu', __('Footer Menu'));
+ register_nav_menus(
+    array(
+      'header-menu' => __( 'Main' ),
+      'footer-menu' => __( 'Footer Menu' )
+    )
+  );
 }
 
 add_action('init', 'register_my_menus');
