b4a24732d4ce25f69f218c76a6ca1cb28e6836e0
Michele <gerunmic@students.zhaw.ch>
2015-01-06 19:10:47 +0100

    Removed hard-coded links

--- wordpress/wp-content/themes/trackstartheme/header.php
+++ wordpress/wp-content/themes/trackstartheme/header.php
@@ -13,11 +13,6 @@
         <h1>Trackstar</h1>
     </div>
         <nav>
-            <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-            <ul>
-                <li><a href="http://localhost:4567/wordpress/?page_id=12">TEST IT</a></li>
-                <li><a href="#">BUY IT</a></li>
-                <li><a href=" http://localhost:4567/wordpress/?page_id=7">TRUST US</a></li>
-            </ul>  
+            <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?> 
         </nav>
     
