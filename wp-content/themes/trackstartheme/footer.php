c8e6b5944e9039cd5b3ebe425e503fb337a4e2c7
des <sachedes@students.zhaw.ch>
2015-01-06 20:55:53 +0100

    inserted contact form

--- wp-content/themes/trackstartheme/footer.php
+++ wp-content/themes/trackstartheme/footer.php
@@ -2,7 +2,9 @@
         <div id="footer">
             <nav>
                 <?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?> 
-            </nav>
+             </nav>
+            
+            <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>
         </div>
     </div>
 </body>
